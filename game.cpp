0ebf807647ceefe24bd879f46b703425db398e9c
Doug Hatcher <doug.hatcher@blueacornici.com>
2021-02-07 02:18:16 -0500

    adds basic input

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include <SDL2/SDL.h>
 #include "game.h"
 #include "graphics.h"
+#include "input.h"
 
 Game::Game() {
     SDL_Init(SDL_INIT_EVERYTHING);
@@ -13,14 +14,27 @@ Game::~Game() {
 
 void Game::gameLoop() {
     Graphics graphics;
+    Input input;
     SDL_Event event;
 
     while(true) {
+        input.beginNewFrame();
         if(SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
+            if(event.type == SDL_KEYDOWN) {
+                if(event.key.repeat == 0) {
+                    input.keyDownEvent(event);
+                }
+            }
+            else if (event.type == SDL_KEYUP) {
+                input.keyUpEvent(event);
+            }
+            else if (event.type == SDL_QUIT) {
                 return;
             }
         }
+        if(input.wasKeyPressed(SDL_SCANCODE_ESCAPE) == true) {
+            return;
+        } 
     }
 
 }

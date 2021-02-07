3970c5790c221208dee3589b617bc62c24c6b966
Doug Hatcher <doug.hatcher@blueacornici.com>
2021-02-07 11:24:44 -0500

    implements graphics files

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 install: ## installs dependancies (debian)
-	sudo apt install libsdl2-gfx-dev fswatch
+	sudo apt install libsdl2-gfx-dev libsdl2-image-dev fswatch
 
 compile: ## compiles the app
-	@g++ *.cpp -lSDL2main -lSDL2 -I./headers
+	@g++ *.cpp -lSDL2main -lSDL2 -lSDL2_image -I./headers
 
 run: compile ## runs the app
 	@./a.out || true
-	
+
 watch: run ## starts with watcher
 	@fswatch -1 * | xargs -n1 make watch
--- headers/graphics.h
+++ headers/graphics.h
@@ -1,16 +1,53 @@
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
+#include <map>
+#include <string>
+
 struct SDL_Window;
 struct SDL_Renderer;
+struct SDL_Rect;
+struct SDL_Texture;
+struct SDL_Surface;
 
 class Graphics {
     public:
         Graphics();
         ~Graphics();
+
+        /**
+         * loads an image into the _spritesheets map if it doesn't already exist 
+         * as a result, each image will only be loaded once. 
+         * Returns the image from the map regardless if image was not just loaded
+         */
+        SDL_Surface* loadImage(const std::string &filePath);
+
+        /**
+         * void BlitSurface
+         * draws a texture to part of the screen
+         */
+        void blitSurface(SDL_Texture* source, SDL_Rect* sourceRectangle, SDL_Rect* destinationRectangle);
+
+        /**
+         * Renders everything to the screen
+         */
+        void flip();
+
+        /**
+         * clears the screen
+         */
+        void clear();
+
+        /**
+         * returns the renderer
+         */
+        SDL_Renderer* getRenderer() const;
+    
     private:
         SDL_Window* _window;
         SDL_Renderer* _renderer;
+
+        std::map<std::string, SDL_Surface*> _spriteSheets;
 };
 
 #endif

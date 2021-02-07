9e1a0b5c971faaa3cbd1dd0040b9ad1704b4d039
Doug Hatcher <doug.hatcher@blueacornici.com>
2021-02-07 15:54:04 -0500

    fleshing out animation objects

--- headers/globals.h
+++ headers/globals.h
@@ -7,5 +7,14 @@ namespace globals {
 
     const int SPRITE_SCALE = 2;   
 }
+struct Vector2 {
+    int x, y;
+
+    Vector2() : x(0), y(0) {}
+    Vector2(int x, int y) : x(x), y(y) {}
+    Vector2 zero() {
+        return Vector2(0, 0);
+    }
+};
 
 #endif
